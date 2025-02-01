205da1b22ba9025be22b3f6dbda61bfb4916e607
Mehmet Emin Şahin <mehmeteminsahin34@gmail.com>
2025-02-01 15:57:43 +0300

    Full App

---
+++ app/src/main/java/com/example/javamaps/adapter/PlaceAdapter.java
@@ -0,0 +1,58 @@
+package com.example.javamaps.adapter;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.javamaps.databinding.RecyclerRowBinding;
+import com.example.javamaps.view.MapsActivity;
+import com.example.javamaps.view.model.Place;
+
+import java.util.List;
+
+public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.PlaceHolder> {
+
+    List<Place> placelist;
+
+    public PlaceAdapter(List<Place> placelist) {
+        this.placelist = placelist;
+    }
+
+    @NonNull
+    @Override
+    public PlaceHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RecyclerRowBinding recyclerRowBinding = RecyclerRowBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
+        return new PlaceHolder(recyclerRowBinding);
+    }
+
+    @Override
+    public int getItemCount() {
+        return placelist.size();
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PlaceHolder holder, int position) {
+        holder.recyclerRowBinding.recyclerViewTextView.setText(placelist.get(position).name);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(holder.itemView.getContext(), MapsActivity.class);
+                intent.putExtra("info","old");
+                intent.putExtra("place",placelist.get(position));
+                holder.itemView.getContext().startActivity(intent);
+            }
+        });
+    }
+
+    public class PlaceHolder extends  RecyclerView.ViewHolder{
+        RecyclerRowBinding recyclerRowBinding;
+        public PlaceHolder(RecyclerRowBinding recyclerRowBinding) {
+            super(recyclerRowBinding.getRoot());
+            this.recyclerRowBinding = recyclerRowBinding;
+        }
+    }
+}
